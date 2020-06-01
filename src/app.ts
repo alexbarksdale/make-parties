86aebffbcd7f657bbd466ae3e8bf9642e629ab55
alexbarksdale <ale.barksdale@gmail.com>
2020-06-01 15:03:56 -0500

    [Add] Create events

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import exphbs from 'express-handlebars';
 import { urlencoded } from 'body-parser';
 
+const models = require('../db/models');
+
 // Initialize express
 const app = express();
 
@@ -12,37 +14,24 @@ app.use(urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-const events = [
-    {
-        title: 'I am your first event',
-        desc: 'A great event that is super fun to look at and good',
-        imgUrl:
-            'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
-    },
-    {
-        title: 'I am your second event',
-        desc: 'A great event that is super fun to look at and good',
-        imgUrl:
-            'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
-    },
-    {
-        title: 'I am your third event',
-        desc: 'A great event that is super fun to look at and good',
-        imgUrl:
-            'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
-    },
-];
-
 app.get('/', (_, res: Response) => {
-    res.render('events-index', { events: events });
+    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then((events: any) => {
+        res.render('events-index', { events: events });
+    });
 });
 
 app.get('/events/new', (_, res: Response) => {
     res.render('events-new', {});
 });
 
-app.post('/events', (req: Request) => {
-    console.log(req.body);
+app.post('/events', (req: Request, res: Response) => {
+    models.Event.create(req.body)
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch((err: any) => {
+            console.log(err);
+        });
 });
 
 const PORT = process.env.PORT || 3000;
