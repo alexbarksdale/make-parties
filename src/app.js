f6ae37689b5eaa56c2cacaf6e7259f66be308d83
alexbarksdale <ale.barksdale@gmail.com>
2020-06-01 18:24:24 -0500

    [Add] RSVP an event

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
-const models = require('../db/models');
+const eventRoutes = require('./routes/events');
 
 // Initialize express
 const app = express();
+
 const hbs = exphbs.create({
     defaultLayout: 'main',
     handlebars: allowInsecurePrototypeAccess(Handlebars),
@@ -17,82 +18,11 @@ const hbs = exphbs.create({
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(eventRoutes);
 
 // View engine setup
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-// GET - Routes
-app.get('/', (_, res) => {
-    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then((events) => {
-        res.render('events-index', { events: events });
-    });
-});
-
-app.get('/events/new', (_, res) => {
-    res.render('events-new', {});
-});
-
-app.get('/events/:id', (req, res) => {
-    models.Event.findByPk(req.params.id)
-        .then((event) => {
-            res.render('events-show', { event: event });
-        })
-        .catch((err) => {
-            console.log(err.message);
-        });
-});
-
-app.get('/events/:id/edit', (req, res) => {
-    models.Event.findByPk(req.params.id)
-        .then((event) => {
-            res.render('events-edit', { event: event });
-        })
-        .catch((err) => {
-            console.log(err.message);
-        });
-});
-
-// POST - Routes
-app.post('/events', (req, res) => {
-    models.Event.create(req.body)
-        .then((event) => {
-            // Redirect to events/:id
-            res.redirect(`/events/${event.id}`);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-// PUT - Routes
-app.put('/events/:id', (req, res) => {
-    models.Event.findByPk(req.params.id)
-        .then((event) => {
-            event
-                .update(req.body)
-                .then(() => {
-                    res.redirect(`/events/${req.params.id}`);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-app.delete('/events/:id', (req, res) => {
-    models.Event.findByPk(req.params.id)
-        .then((event) => {
-            event.destroy();
-            res.redirect('/');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ src/routes/events.js
@@ -0,0 +1,96 @@
+const express = require('express');
+
+const models = require('../../db/models');
+
+const router = express.Router();
+
+// GET - Routes
+router.get('/', (_, res) => {
+    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then((events) => {
+        res.render('events-index', { events: events });
+    });
+});
+
+router.get('/events/new', (_, res) => {
+    res.render('events-new', {});
+});
+
+router.get('/events/:id', (req, res) => {
+    models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] })
+        .then((event) => {
+            res.render('events-show', { event: event });
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+});
+
+router.get('/events/:id/edit', (req, res) => {
+    models.Event.findByPk(req.params.id)
+        .then((event) => {
+            res.render('events-edit', { event: event });
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+});
+
+router.get('/events/:eventId/rsvps/new', (req, res) => {
+    models.Event.findByPk(req.params.eventId).then((event) => {
+        res.render('rsvps-new', { event: event });
+    });
+});
+
+// POST - Routes
+router.post('/events', (req, res) => {
+    models.Event.create(req.body)
+        .then((event) => {
+            // Redirect to events/:id
+            res.redirect(`/events/${event.id}`);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+router.post('/events/:eventId/rsvps', (req, res) => {
+    req.body.EventId = req.params.eventId;
+    models.Rsvp.create(req.body)
+        .then((rsvp) => {
+            res.redirect(`/events/${req.params.eventId}`);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// PUT - Routes
+router.put('/events/:id', (req, res) => {
+    models.Event.findByPk(req.params.id)
+        .then((event) => {
+            event
+                .update(req.body)
+                .then(() => {
+                    res.redirect(`/events/${req.params.id}`);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+router.delete('/events/:id', (req, res) => {
+    models.Event.findByPk(req.params.id)
+        .then((event) => {
+            event.destroy();
+            res.redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+module.exports = router;
